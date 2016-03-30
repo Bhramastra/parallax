ede43e55f9c8063f2c69b2b1c08d89c398648737
Gaurav Shukla <gshukla66@gmail.com>
2016-03-30 13:22:08 +0530

    changes

---
+++ master.py
@@ -0,0 +1,7 @@
+import msg
+
+# Start master server
+msg.server(1200,type='master')
+
+
+__author__ = 'gaurav'
--- parallax-reports.py
+++ parallax-reports.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify,request
+from flask import Flask,jsonify,request,render_template
 from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
@@ -16,26 +16,55 @@ class Node(db.Model):
     ip = db.Column(db.String(80), unique=True)
     mem = db.Column(db.Float)
     cpu = db.Column(db.Float)
+    status = db.Column(db.String(20))
 
     def __init__(self,ip,mem,cpu):
         self.ip = ip
         self.cpu = cpu
         self.mem = mem
+        self.status= online
+
     def json(self):
         data={}
         data['ip']=self.ip
         data['cpu']=self.cpu
         data['mem']=self.mem
         data['id']=self.id
+        data['status']= self.status
         return data
 
 admin.add_view(ModelView(Node, db.session))
 
 db.create_all()
 
+
+@app.route('/restart/<id>',methods=["GET"])
+@app.route('/shutdown/<id>',methods=["GET"])
+@app.route('/eot/<id>',methods=["GET"])
+def action(id):
+    id=int(id)
+    node=Node.query.get(id)
+    if node is None:
+        return jsonify(error="Node not found"),404
+    rule=request.url_rule
+    if 'restart' in rule.rule:
+        command=1
+        node.status="Restarting"
+    elif 'shutdown' in rule.rule:
+        command=2
+        node.status="Shutting Down"
+    elif 'eot' in rule.rule:
+        command=3
+        node.status="Killing all tasks"
+    db.session.commit()
+    node.send()
+
+
+
 @app.route('/manage',methods=["GET"])
 def manage():
-    return render_template()
+    nodes=Node.query.all()
+    return render_template('manage.html',nodes=nodes)
 
 @app.route('/stat/<int:id>',methods=["GET","PUT","POST","DELETE"])
 @cross_origin(origin='*', headers=['Content- Type', 'Authorization'])
@@ -46,6 +75,8 @@ def stat(id):
     if request.method =='PUT':
         node.cpu=request.form['cpu']
         node.mem=request.form['mem']
+        if 'status' in request.form:
+            node.status=request.form['status']
         db.session.commit()
         return "Updated",200
     elif request.method == 'DELETE':
