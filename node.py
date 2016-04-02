05f48af9420e48938ef2f034f311e887bb539ba0
Gaurav Shukla <gshukla66@gmail.com>
2016-04-02 15:35:05 +0530

    Replaced thread with Threading module :)

--- node.py
+++ node.py
@@ -3,8 +3,8 @@ import subprocess
 import psutil
 import os
 import requests
-import thread
-
+import threading
+from Queue import Queue
 
 command=0
 used_mem = psutil.virtual_memory()
@@ -32,25 +32,28 @@ id = k['id']
 
 
 
-def reportStat():
-    while 1:
-        if command== -1:
-            print "Closing"
-            thread.exit()
-        used_mem = psutil.virtual_memory()
-        mem = 100 - used_mem[2]
-        os.system("mpstat > cpuse.txt")
-        file = open("cpuse.txt","r")
-        count = 0
-        for line in file:
-            count = count + 1
-            if count==4:
-                val=line.split()
-        cpu_usage =  val[3]
-        cpu = float(cpu_usage)
-        file.close()
-        payload = {"cpu" : cpu ,"mem" : mem }
-        r = requests.put("http://localhost:5000/stat/"+ str(id),payload)
+def reportStat(q):
+    try:
+        while 1:
+            if command== -1:
+                print "Closing"
+                thread.exit()
+            used_mem = psutil.virtual_memory()
+            mem = 100 - used_mem[2]
+            os.system("mpstat > cpuse.txt")
+            file = open("cpuse.txt","r")
+            count = 0
+            for line in file:
+                count = count + 1
+                if count==4:
+                    val=line.split()
+            cpu_usage = val[3]
+            cpu = float(cpu_usage)
+            file.close()
+            payload = {"cpu" : cpu ,"mem" : mem }
+            r = requests.put("http://localhost:5000/stat/"+ str(id),payload)
+    except Exception as e:
+        q.put((repr(e), threading.current_thread.func_name))
 
 
 def shutdown(s):
@@ -64,40 +67,75 @@ def shutdown(s):
     s.close()
 
 
-def recvfile():
-    print "recvfile started"
-    sock = socket.socket()
-    sock.connect(('0.0.0.0', 1201))
-    cloud_code = open("cloudcode.py","w")
-    while 1:
-        data = sock.recv(1)
-        if data:
-            print "data from test.py \t"+data
-            if data=='$':
-                data=sock.recv(4)
-                if data=="exit":
-                    cloud_code.close()
+def recvfile(q):
+    try:
+        print "recvfile started"
+        sock = socket.socket()
+        sock.connect(('0.0.0.0', 1201))
+        cloud_code = open("cloudcode.py","w")
+        while 1:
+            data = sock.recv(1)
+            if data:
+                print "data from test.py \t"+data
+                if data=='$':
+                    data=sock.recv(4)
+                    if data=="exit":
+                        cloud_code.close()
+                    else:
+                        cloud_code.write("$"+data)
                 else:
-                    cloud_code.write("$"+data)
-            else:
-                cloud_code.write(data)
+                    cloud_code.write(data)
+    except Exception as e:
+        q.put((repr(e), threading.current_thread.func_name))
+        return
+
+def master_listener(q):
+    try:
+        s = socket.socket()
+        s.connect(('0.0.0.0', 1200))
+        print "Master Listener started"
+        while 1:
+            data = s.recv(1024)
+            print data
+            if data == '1':
+                shutdown(s)
+            if data == '2':
+                restart()
+            if data=='3':
+                kill_all()
+            if 'exec' in data:
+                #create new file agent.py
+                #exec printcval(a,b,c)
+                os.system("pyhton agent.py")
+    except Exception as e:
+        q.put((repr(e), threading.current_thread().getName()))
+        return
 
 
-thread.start_new_thread(reportStat,())
-thread.start_new_thread(recvfile,())
-s=socket.socket()
-s.connect(('0.0.0.0',1200))
+def logger(q):
+    print "Logger Thread started"
+    try:
+        while 1:
+            data = q.get()
+            print data
+    except Exception as e:
+        print repr(e)
+        return
+
+
+# Global queue where all threads leave there errors
+q = Queue()
+
+threadlist = [threading.Thread(target=reportStat, args=(q,), name="Report Stat"),
+            threading.Thread(target=recvfile, args=(q,), name="File Receiver"),
+            threading.Thread(target=master_listener, args=(q,), name="Master listener"),
+            threading.Thread(target=logger, args=(q,), name="Logger")]
+
+
+for t in threadlist:
+    t.setDaemon(True)
+    t.start()
+
 while 1:
-    data=s.recv(1024)
-    print data
-    if data=='1':
-        shutdown(s)
-    if data=='2':
-        restart()
-    if data=='3':
-        kill_all()
-    if 'exec' in data:
-        #create new file agent.py
-        #exec printcval(a,b,c)
-        os.system("pyhton agent.py")
+    pass
 
